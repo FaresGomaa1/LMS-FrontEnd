6be018659a04fd8d8b628bf7d4ba83b7207b4b0e
FaresGomaa1 <faresgomaa336@gmail.com>
2024-04-01 12:52:01 +0200

    something

--- src/app/instructor/components/core/instructor-courses/instructor-courses.component.ts
+++ src/app/instructor/components/core/instructor-courses/instructor-courses.component.ts
@@ -67,4 +67,9 @@ export class InstructorCoursesComponent implements OnInit {
       }
     );
   }
+  openMaterialtLink(course: ICourse): void {
+ 
+    window.open(course.material, '_blank');
+  
+  }
 }
