1b81f3fd495bd5c0d670e7a61813788c4d7a3263
Marina Fouad <108586666+MarinaFouad@users.noreply.github.com>
2024-04-01 13:34:28 +0200

    Update course.service.ts

--- src/app/instructor/service/course.service.ts
+++ src/app/instructor/service/course.service.ts
@@ -1,7 +1,8 @@
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpParams } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable, of } from 'rxjs';
+import { catchError, map, tap } from 'rxjs/operators';
 import { ICourse } from '../interface/i-course';
-import { Observable, map } from 'rxjs';
 import { IStudent } from '../interface/istudent';
 
 @Injectable({
@@ -9,34 +10,66 @@ import { IStudent } from '../interface/istudent';
 })
 export class CourseService {
   baseURL: string = "http://localhost:5050/Courses";
+
   constructor(private http: HttpClient) { }
 
   getAllCourses(): Observable<ICourse[]> {
-    return this.http.get<ICourse[]>(`${this.baseURL}`)
-  }
-  getCourseById(ID: number): Observable<ICourse> {
-    return this.http.get<ICourse>(`${this.baseURL}/${ID}`)
+    return this.http.get<ICourse[]>(this.baseURL);
   }
 
-  getCourseByName(name:string): Observable<ICourse> {
-    return this.http.get<ICourse>(`${this.baseURL}/${name}`)
+  getCourseById(id: number): Observable<ICourse> {
+    return this.http.get<ICourse>(`${this.baseURL}/${id}`);
   }
 
-  addCourse(course: ICourse, instructorId: number): Observable<ICourse> {
-    return this.http.post<ICourse>(`${this.baseURL}`, course, { params: { instructorId: instructorId.toString() } });
-  }
- 
- 
-  updateCourse(course: ICourse , id : number): Observable<any> {
-    return this.http.put(`${this.baseURL}/${id}`, course );
+//   Add(course: FormData) {
+//     return this.http
+//         .post(this.baseURL, course)
+//         .pipe(
+//             tap(() => {
+//                 this.newCourseAdded.next();
+//             })
+//         );
+// }
+  getCourseByName(name: string): Observable<ICourse[]> {
+    const params = new HttpParams().set('name', name);
+    return this.http.get<ICourse[]>(this.baseURL, { params });
   }
 
-  deleteCourse(ID: number): Observable<ICourse> {
-    return this.http.delete<ICourse>(`${this.baseURL}/${ID}` );
+  // addCourse(course: ICourse, instructorId: number): Observable<ICourse> {
+  //   return this.http.post<ICourse>(this.baseURL, course, { params: { instructorId: instructorId.toString() } });
+  // }
+
+  addCourse(course: FormData, instructorId: number): Observable<ICourse> {
+    // Append instructorId as a query parameter
+    const params = new HttpParams().set('instructorId', instructorId.toString());
+    return this.http.post<ICourse>(this.baseURL, course, { params });
   }
-  getStudentsEnrolledInCourse(courseId: number): Observable<IStudent[]> {
-    return this.http.get<IStudent[]>(`${this.baseURL}/Student`).pipe(
-      map(students => students.filter(student => student.courseIDs.includes(courseId)))
+  updateCourse(course: ICourse, id: number): Observable<ICourse> {
+    return this.http.put<ICourse>(`${this.baseURL}/${id}`, course);
+  }
+
+  deleteCourse(id: number): Observable<void> {
+    return this.http.delete<void>(`${this.baseURL}/${id}`);
+  }
+
+  getAllStudents(): Observable<IStudent[]> { 
+    return this.http.get<IStudent[]>(`http://localhost:5050/Student`);
+  }
+
+  getCourseEnrolledStudentsCount(courseName: string): Observable<number> {
+    return this.getAllStudents().pipe(
+      map(students => {
+        let count = 0;
+        students.forEach(student => {
+          if (student.courseName && student.courseName.includes(courseName)) {
+            count++;
+          }
+        });
+        return count;
+      }),
+      catchError(error => {
+        console.error('Failed to get students:', error);
+        return of(0); // Return an observable emitting 0 in case of error
+      })
     );
-  }
-}
+  }}
